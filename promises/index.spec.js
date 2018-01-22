9833ca2c21d3f9541a3ce4710b659b2bcd08f97b
amangalvedhekar <amangalvedhekar@onist.com>
2018-01-21 19:31:14 -0500

    unit test for promise simple example

--- promises/index.spec.js
+++ promises/index.spec.js
@@ -2,6 +2,8 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 
 import { promiseObject } from './index';
+
+import { gamesList } from '../mock-data/arrayObjects';
 //fat arrow will fail to bind `this`
 describe('promises', function () {
 
@@ -13,6 +15,14 @@ describe('promises', function () {
     const incorrectRequestData = () => promiseObject()
             .catch(err => assert.equal(err.message, 'requestData not provided'));
 
+    const psnRequestData = () => {
+        const requestObect = {
+            psnExclusive: true
+        };
+        return promiseObject(requestObect)
+            .then(data => assert.deepEqual(data, gamesList));
+    };
+
 
 
     /********************************************************************************
@@ -21,4 +31,5 @@ describe('promises', function () {
      */
 
     it('should throw error if request data is not sent', incorrectRequestData);
+    it('should send an array of psn Exclusive list', psnRequestData);
 });

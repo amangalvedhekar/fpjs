84f5ffc3d5d89b0ea650c61b0f02a1022d3f1a8a
Anirudh Mangalvedhekar <amangalvedhekar@icloud.com>
2018-04-19 01:52:24 -0400

    arrow function example (#2)

---
+++ es6Beyond/arrow-function/index.js
@@ -0,0 +1,28 @@
+import {
+  gamesList,
+} from '../../mock-data/arrayObjects';
+
+function getGameNames(gameObject) {
+  /*
+  * arguments object cannot be accessed with arrow function but
+  * rest parameters can be used
+  * */
+
+  console.log(arguments.length, 'inside anonymous function');
+  return gameObject.name;
+}
+
+const getNamesList  = game => game.name;
+const restParameter = (...game) => {
+  console.log(game.length, 'inside new arrow funct');
+  return game[0].name;
+};
+//Anonymous function
+const gameNames = gamesList.map(getGameNames);
+
+//Arrow Function
+const gameNamesList = gamesList.map(getNamesList);
+
+const restExample = gamesList.map(restParameter);
+
+console.log(gameNamesList, gameNames, restExample);

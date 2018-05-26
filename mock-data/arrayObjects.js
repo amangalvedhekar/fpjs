f16f035bd52eeb857a655898eab4300aab08370f
Anirudh <anirudh.mangalvedhekar1989@gmail.com>
2018-05-26 03:07:11 -0400

    fix

--- mock-data/arrayObjects.js
+++ mock-data/arrayObjects.js
@@ -1,29 +1,29 @@
 const gamesList = [
-    {
-        id:1,
-        genre: 'action',
-        name: 'Killzone'
-    },
-    {
-        id:2,
-        genre: 'action',
-        name: 'Resistance'
-    },
-    {
-        id: 3,
-        genre: 'action',
-        name: 'Infamous'
-    },
-    {
-        id: 4,
-        genre: 'action',
-        name: 'God Of War!!!'
-    }
-    ];
+  {
+    id: 1,
+    genre: 'action',
+    name: 'Killzone'
+  },
+  {
+    id: 2,
+    genre: 'action',
+    name: 'Resistance'
+  },
+  {
+    id: 3,
+    genre: 'action',
+    name: 'Infamous'
+  },
+  {
+    id: 4,
+    genre: 'action',
+    name: 'God Of War!!!'
+  }
+];
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
-const filterList = Array.from({ length: 40 }, (_,index) => index +1);
+const filterList = Array.from({ length: 40 }, (_, index) => index + 1);
 
 export {
-    gamesList,
-    filterList
+  gamesList,
+  filterList
 };
